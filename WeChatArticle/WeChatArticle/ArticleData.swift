699d2b00ca38f198b5cde625c97ca7ac44f11664
broven <zycode277@163.com>
2016-02-25 22:54:16 +0800

    完成将json格式化

--- WeChatArticle/WeChatArticle/ArticleData.swift
+++ WeChatArticle/WeChatArticle/ArticleData.swift
@@ -9,9 +9,9 @@
 import Foundation
 import Alamofire
 import SwiftyJSON
+var id = 0
 class ArticleData  {
-    
-    class func getJson(returnArticleNumber num :Int = 10, randomOrNot : Bool = true , word:String = "盗墓笔记" , page :Int =  1) {
+        class func getJson(returnArticleNumber num :Int = 10, randomOrNot : Bool = true , word:String = "盗墓笔记" , page :Int =  1) {
         var rand : Int
         if randomOrNot == false{
             rand = 0
@@ -23,17 +23,34 @@ class ArticleData  {
     
         Alamofire.request(.GET,"http://apis.baidu.com/txapi/weixin/wxhot?num=10&rand=1&word=%E7%9B%97%E5%A2%93%E7%AC%94%E8%AE%B0&page=1", headers : headers,parameters : ["num" : num,"rand" : rand , "word": word,"page" : page]).responseJSON() {
         response in
-            if let i = response.result.value{
-            let json = JSON(i)
-            parseJSONToStruct(<#T##ArticleData#>)
-                        }
+            switch response.result
+            {
+            case .Success:
+                if let value = response.result.value
+                {
+                    let json = JSON(value)
+                     parseJSONToStruct(json,num: num)
+                }
+            case .Failure(let error):
+                print(error)
             }
-        
-    }
-    private func parseJSONToStruct(json : JSON){
-        let type = json["newslist"][1]["title"].stringValue
-        print("say:",type)
+                                    }
 
+}
+    private class func parseJSONToStruct(json : JSON , num : Int) -> [Article]{
+        var ArticleList : [Article] = []
+        for var index = 0 ; index < num; ++index{
+            let title = json["newslist"][index]["title"].stringValue
+            let description = json["newslist"][index]["description"].stringValue
+            let picUrl = json["newslist"][index]["picUrl"].stringValue
+            let url = json["newslist"][index]["url"].stringValue
+            let art = Article(title: title, description: description, picUrl: picUrl, url: url)
+            ArticleList.append(art)
+            
         
+            //print("say:",type)
+        }//loop
+        return ArticleList
     }
+
 }
